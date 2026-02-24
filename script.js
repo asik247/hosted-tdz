62cfaebea7b826ca5404aad4da7f18944ee3574c
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-24 12:01:56 +0600

    first commit this repo

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// Hosted :-var,let,const all variable hosted hoy;
+console.log(a);
+var a = 20;
+/**
+ * var a undifind return kore karon hosted hoy
+ */
+sayHi();
+function sayHi(){
+    console.log("iam funk");
+}
+/**
+ * Funk fully hosted hoy
+ */
+console.log(l);
+let l = "iam let";
+/**
+ * hosted but insitial korer age access kora jai na, reference error show kore and TDZ
+ */
+
+
+
